dc6dab53218781e5292dfc865b9a5b2e4ab350fa
George Shinkarev <gkshi@ya.ru>
2019-01-28 06:33:42 +0300

    [ADD] design one

--- posts/islands/demo/js/index.js
+++
@@ -1,8 +0,0 @@
-(function (root) {
-    var map = root.SHRI_ISLANDS.MAP;
-    var count = root.SHRI_ISLANDS.solution(map);
-
-    document.querySelector('.outer').appendChild(
-        root.SHRI_ISLANDS.render(map, count)
-    );
-})(this);
